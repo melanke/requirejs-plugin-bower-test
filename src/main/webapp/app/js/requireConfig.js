8633f5efa0c88f7ed09d080208068d35592f5e95
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-05-07 20:16:21 -0300

    first commit

---
+++ src/main/webapp/app/js/requireConfig.js
@@ -0,0 +1,15 @@
+requirejs.config({
+    bower: {
+        root: "../../../bower.json",
+        baseUrl: '../../../bower_components',
+        extensions: 'js|css',
+        ignore: 'requirejs|requirejs-domready|requirejs-text',
+        auto: true,
+        deps: ['dependencies'] // can add 'devDependencies' as well
+    },
+    paths: {
+        bower: '../../../bower_components/requirejs-plugin-bower/src/bower',
+        json: '../../../bower_components/requirejs-plugins/src/json',
+        text: '../../../bower_components/requirejs-text/text'
+    }
+});
